bd70a6c0d148e58e564d305ed305a0aff92ca370
Nelamin2 <nidalelamin@gmail.com>
2024-06-27 00:16:37 +0300

    tasks1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success)
+{
+return new promise ((resolve, reject) => {
+if success
+{
+resolve({status:200,
+        body: 'success',
+});
+}else
+{
+reject(new Error (The fake API is not working currently));
+}
+});
+}
